2dfc6953969f9a883c0c91533ae596cd43e0f018
Zachary S Cacciatore <zachcacc1223@gmail.com>
2017-12-20 22:50:13 -0600

    Create mapper.py

---
+++ Project1-pt2/most-popular-path/mapper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# Q3: most popular
+
+import sys
+import re # library of regular expression operations
+
+for line in sys.stdin:
+    data = line.strip().split(" ")
+    if len(data) == 10:
+        ip_address, identity, username, datetime, timezone, method, path, proto, status, size = data
+        cleaned_path = re.sub(r"^http://www.the-associates.co.uk", '', path)
+        print cleaned_path
