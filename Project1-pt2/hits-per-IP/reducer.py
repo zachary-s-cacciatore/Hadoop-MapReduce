bacb360f393214089ed51a94e272df84d620681a
Zachary S Cacciatore <zachcacc1223@gmail.com>
2017-12-20 22:48:03 -0600

    Create reducer.py

---
+++ Project1-pt2/hits-per-IP/reducer.py
@@ -0,0 +1,26 @@
+#!usr/bin/python
+
+
+# Q2: hits to page
+
+import sys
+
+hitsTotal = 0
+oldIP = None
+
+# q2: hits from IP
+
+for line in sys.stdin:
+    thisIP = line.strip().split()
+    if len(thisIP) != 1:
+        continue
+
+    if oldIP and oldIP != thisIP:
+        print oldIP, "\t", hitsTotal
+        hitsTotal = 0
+        
+    oldIP = thisIP
+    hitsTotal += 1
+
+if oldIP != None:
+    print oldIP, "\t", hitsTotal
