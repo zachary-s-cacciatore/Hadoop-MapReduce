a539118c163129d2c0b26a22956d437899c24f54
Zachary S Cacciatore <zachcacc1223@gmail.com>
2017-12-20 19:54:17 -0600

    Create mapper.py

---
+++ Project1/sales-per-category/mapper.py
@@ -0,0 +1,11 @@
+#!usr/bin/python
+
+# Q2: highest sale
+
+import sys
+
+for line in sys.stdin:
+    data = line.strip().split("\t")
+    if len(data) == 6:
+        date, time, store, item, cost, payment = data
+        print "{0}\t{1}".format(store, cost)
