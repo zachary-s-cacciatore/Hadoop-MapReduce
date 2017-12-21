e7004d8858899e1557873a8a157792e9998e441f
Zachary S Cacciatore <zachcacc1223@gmail.com>
2017-12-20 20:01:25 -0600

    Create mapper.py

---
+++ Project1/highest-sale/mapper.py
@@ -0,0 +1,12 @@
+#!usr/bin/python
+
+# Format of each line is:
+# date\ttime\tstore name\titem description\tcost\tmethod of payment
+
+import sys
+
+for line in sys.stdin:
+    data = line.strip().split("\t")
+    if len(data) == 6:
+      date, time, store, item, cost, payment = data
+      print "{0}\t{1}".formate(store, cost)
